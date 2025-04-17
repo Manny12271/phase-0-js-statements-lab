21374ceea93935a3c3a8330348ac34e43d03a80c
manuel alfaro <manuel.alfaro247@gmail.com>
2025-04-17 16:07:03 -0500

    Garden Management

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,22 @@
-const temperature = 90;
-const timeOfDay = "morning";
+// gardenManagement.js
+
+// Make sure values match what the tests expect
+const temperature = 85; // Must be > 80 for "Watering on"
+if (temperature > 80) {
+  console.log("Watering on");
+} else {
+  console.log("Watering off");
+}
+
+const timeOfDay = "morning"; // Must be "morning" for "Lights off"
+if (timeOfDay === "evening" || timeOfDay === "night") {
+  console.log("Lights on");
+} else {
+  console.log("Lights off");
+}
+
 let soilMoisture = 30;
+while (soilMoisture < 40) {
+  soilMoisture++;
+}
+console.log(soilMoisture); // Should log 40
